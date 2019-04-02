a3a73d8ad05bc79b5271169de5e51138078cc06b
chcbaram <chcbaram@paran.com>
2019-04-02 21:35:22 +0900

    배터리/리모콘 시험 코드 추가

---
+++ examples/EduBot_Main/src/Battery.cpp
@@ -0,0 +1,21 @@
+#include <EduBot.h>
+
+
+
+void battery_setup() {
+  // put your setup code here, to run once:
+  
+  edubot.begin(115200);   
+}
+
+void battery_loop() {
+  // put your main code here, to run repeatedly
+  float vol;
+
+  edubot.lcd.clearDisplay();
+  edubot.lcd.printf(0, 0, "Battery Voltage");
+  vol = edubot.batteryGetVoltage();
+  edubot.lcd.printf(0, 16,"%1.1f V", vol/10);
+  edubot.lcd.display();
+  delay(100);
+}
---
+++ examples/EduBot_Main/src/IrRemote.cpp
@@ -0,0 +1,30 @@
+#include <EduBot.h>
+
+
+
+void irremote_setup() {
+  // put your setup code here, to run once:
+  
+  edubot.begin(115200);   
+  edubot.tofEnd();                                
+}
+
+void irremote_loop() {
+  // put your main code here, to run repeatedly
+
+  if (edubot.ir_remote.available() > 0)
+  {
+    uint32_t key_code;
+
+    key_code = edubot.ir_remote.read();
+
+    Serial.print("Key : 0x");
+    Serial.print(key_code, HEX);
+    Serial.print(" ");
+    Serial.println(edubot.ir_remote.getTime());
+    edubot.lcd.clearDisplay();
+    edubot.lcd.printf(0, 0, "KeyCode");
+    edubot.lcd.printf(0, 16,"0x%X", key_code);
+    edubot.lcd.display();
+  }
+}
